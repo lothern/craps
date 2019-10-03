e20ae7ab026c351cba39af125cef74d52afed71b
Jason Woodard <jwoodard@jwoodard-macbookpro1.roam.corp.google.com>
2019-10-03 09:25:16 -0400

    Simplify isPointOn

--- src/craps-table.ts
+++ src/craps-table.ts
@@ -3,86 +3,80 @@ import { Dice, LiveDice } from './dice/dice';
 import * as _ from 'lodash';
 
 export class CrapsTable {
-  
+
   public currentPoint: number
 
   public dice: Dice;
-  
+
   private _isPointOn: boolean = false;
-  
+
   private _bets: Bet[] = [];
   bettors: ((table: CrapsTable) => void)[];
-  
-  constructor(){
+
+  constructor() {
     this.dice = new LiveDice();
     this.bettors = [];
   }
-  
+
   get isPointOn(): boolean {
-    return this.checkPointIsOn();
+    return !(this.currentPoint == undefined);
   };
 
-  private checkPointIsOn() : boolean {
-    if (this.currentPoint == undefined) {
-      return false;
-    }
-    return true;
-  }
-  
-  getLastRoll() {
+  getLastRoll() : number {
     return _.last(this.dice.rollHistory);
   }
 
-  placeBet(bet: Bet) : void {
+  placeBet(bet: Bet): void {
     this._bets.push(bet);
   };
 
-  get bets() : Bet[] {
+  get bets(): Bet[] {
     return this._bets;
   };
-  
+
   onPlaceBets(bettor: (table: CrapsTable) => void) {
     this.bettors.push(bettor);
   }
 
-  getPlayerBets(playerId: string) : Bet[] {
+  getPlayerBets(playerId: string): Bet[] {
     let playerBets = _.filter(this._bets, bet => {
       return bet.player == playerId;
     });
     return _.clone(playerBets);
   }
 
-  rollDice() : void {
+  rollDice(): void {
 
     // TODO: Add player table load logging right before dice roll.
     // How much each player has on the table (# of bets and total amount)
-      
+
     // Roll dice and resolve bets.
     let rollvalue = this.dice.roll();
 
     // Resolve the bets
     this.resolveBets(rollvalue);
-    
-    if (this.checkPointIsOn()) {
-      if (this.currentPoint === rollvalue 
+
+    // 
+    if (this.isPointOn) {
+      if (this.currentPoint === rollvalue
         || rollvalue === 7) {
         this.currentPoint = undefined;
-      } 
+      }
     } else {
-      if (rollvalue >= 4 && rollvalue <= 6 || 
-        rollvalue >= 8 && rollvalue <=10) {
+      if (rollvalue >= 4 && rollvalue <= 6 ||
+        rollvalue >= 8 && rollvalue <= 10) {
         this.currentPoint = rollvalue;
       }
     }
   }
-  
+
   resolveBets(rollValue: number) {
 
     switch (rollValue) {
       case 2:
       case 3:
       case 12:
-        if(!this.checkPointIsOn()) {
+        if (!this.isPointOn) {
           this._bets.forEach(bet => {
             bet.lose();
           });
@@ -90,7 +84,7 @@ export class CrapsTable {
         this._bets = [];
         break;
       case 7:
-        if(this.checkPointIsOn()) {
+        if (this.isPointOn) {
           // Seven Out
           this._bets.forEach(bet => {
             bet.lose();
@@ -103,7 +97,7 @@ export class CrapsTable {
         }
         break;
       case 11:
-        if (!this.checkPointIsOn()) {
+        if (!this.isPointOn) {
           // 11 on Comeout
           this._bets.forEach(bet => {
             bet.win(this);
